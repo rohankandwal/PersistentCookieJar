d07ad6d8d382abfaecd8772f58fa1cba921e8e94
Rohan Kandwal <rohan@nexialabs.com>
2017-05-03 17:04:50 +0530

    Added method to get cookie information on demand

--- okhttp3-persistent-cookiejar/src/main/java/com/franmontiel/persistentcookiejar/PersistentCookieJar.java
+++ okhttp3-persistent-cookiejar/src/main/java/com/franmontiel/persistentcookiejar/PersistentCookieJar.java
@@ -16,85 +16,113 @@
 
 package com.franmontiel.persistentcookiejar;
 
+import android.text.TextUtils;
 import com.franmontiel.persistentcookiejar.cache.CookieCache;
 import com.franmontiel.persistentcookiejar.persistence.CookiePersistor;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import okhttp3.Cookie;
 import okhttp3.HttpUrl;
 
 public class PersistentCookieJar implements ClearableCookieJar {
 
-    private final CookieCache cache;
-    private final CookiePersistor persistor;
-    private final boolean ignorePersistance;
+  private final CookieCache cache;
+  private final CookiePersistor persistor;
+  private final boolean ignorePersistance;
 
-    public PersistentCookieJar(CookieCache cache, CookiePersistor persistor, boolean ignorePersistance) {
-        this.cache = cache;
-        this.persistor = persistor;
-        this.ignorePersistance = ignorePersistance;
+  public PersistentCookieJar(
+    CookieCache cache,
+    CookiePersistor persistor,
+    boolean ignorePersistance
+  ) {
+    this.cache = cache;
+    this.persistor = persistor;
+    this.ignorePersistance = ignorePersistance;
 
-        this.cache.addAll(persistor.loadAll());
+    this.cache.addAll(persistor.loadAll());
+  }
+
+  @Override
+  synchronized public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+    cache.addAll(cookies);
+    persistor.saveAll(filterPersistentCookies(cookies, ignorePersistance));
+  }
+
+  private static List<Cookie> filterPersistentCookies(
+    List<Cookie> cookies,
+    boolean ignorePersistance
+  ) {
+    List<Cookie> persistentCookies = new ArrayList<>();
+
+    if (ignorePersistance) {
+      return cookies;
     }
-
-    @Override
-    synchronized public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
-        cache.addAll(cookies);
-        persistor.saveAll(filterPersistentCookies(cookies, ignorePersistance));
+    for (Cookie cookie : cookies) {
+      if (cookie.persistent()) {
+        persistentCookies.add(cookie);
+      }
     }
+    return persistentCookies;
+  }
 
-    private static List<Cookie> filterPersistentCookies(List<Cookie> cookies, boolean ignorePersistance) {
-        List<Cookie> persistentCookies = new ArrayList<>();
-
-        if (ignorePersistance) {
-          return cookies;
+  /**
+   * Function to get saved cookie
+   *
+   * @param url String containing url of the website for which cookie was saved
+   * @param cookieName Name of the cookie you want from saved cookies
+   * @return Cookie
+   */
+  public String getCookie(final String url, final String cookieName) {
+    if (TextUtils.isEmpty(url) || TextUtils.isEmpty(cookieName)) {
+      return null;
+    }
+    HttpUrl httpUrl = new HttpUrl.Builder().host(url).build();
+    final List<Cookie> cookieList = loadForRequest(httpUrl);
+    if (cookieList != null && cookieList.size() > 0) {
+      for (final Cookie cookieInfo : cookieList) {
+        if (cookieInfo.name().equalsIgnoreCase(cookieName)) {
+          return cookieInfo.value();
         }
-        for (Cookie cookie : cookies) {
-            if (cookie.persistent()) {
-                persistentCookies.add(cookie);
-            }
-        }
-        return persistentCookies;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  synchronized public List<Cookie> loadForRequest(HttpUrl url) {
+    List<Cookie> cookiesToRemove = new ArrayList<>();
+    List<Cookie> validCookies = new ArrayList<>();
+
+    for (Iterator<Cookie> it = cache.iterator(); it.hasNext(); ) {
+      Cookie currentCookie = it.next();
+
+      if (isCookieExpired(currentCookie)) {
+        cookiesToRemove.add(currentCookie);
+        it.remove();
+      } else if (currentCookie.matches(url)) {
+        validCookies.add(currentCookie);
+      }
     }
 
-    @Override
-    synchronized public List<Cookie> loadForRequest(HttpUrl url) {
-        List<Cookie> cookiesToRemove = new ArrayList<>();
-        List<Cookie> validCookies = new ArrayList<>();
+    persistor.removeAll(cookiesToRemove);
 
-        for (Iterator<Cookie> it = cache.iterator(); it.hasNext(); ) {
-            Cookie currentCookie = it.next();
+    return validCookies;
+  }
 
-            if (isCookieExpired(currentCookie)) {
-                cookiesToRemove.add(currentCookie);
-                it.remove();
+  private static boolean isCookieExpired(Cookie cookie) {
+    return cookie.expiresAt() < System.currentTimeMillis();
+  }
 
-            } else if (currentCookie.matches(url)) {
-                validCookies.add(currentCookie);
-            }
-        }
+  @Override
+  synchronized public void clearSession() {
+    cache.clear();
+    cache.addAll(persistor.loadAll());
+  }
 
-        persistor.removeAll(cookiesToRemove);
-
-        return validCookies;
-    }
-
-    private static boolean isCookieExpired(Cookie cookie) {
-        return cookie.expiresAt() < System.currentTimeMillis();
-    }
-
-    @Override
-    synchronized public void clearSession() {
-        cache.clear();
-        cache.addAll(persistor.loadAll());
-    }
-
-    @Override
-    synchronized public void clear() {
-        cache.clear();
-        persistor.clear();
-    }
+  @Override
+  synchronized public void clear() {
+    cache.clear();
+    persistor.clear();
+  }
 }
